78bf9bd1951b80b817d3af5c350d0bc07fa00233
Ali chatraei <m.saeri@fanap.ir>
2021-09-01 10:40:22 +0430

    Import Grid layout from Material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { Grid } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <Grid container direction="column">
+      <Grid item>This is header</Grid>
+      <Grid item>This is content</Grid>
+    </Grid>
   );
 }
 

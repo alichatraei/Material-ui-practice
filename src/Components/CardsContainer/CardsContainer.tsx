6d86ea58c0abc46e02e26ae3d86b20070abcadf2
Ali chatraei <m.saeri@fanap.ir>
2021-09-01 11:06:19 +0430

    Create card container to show cards

---
+++ src/Components/CardsContainer/CardsContainer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card, Typography, CardActions, CardContent, Button, makeStyles } from '@material-ui/core'
+const useStyles = makeStyles({
+    root: {},
+    title: {},
+    pos: {}
+})
+const CardsContainer = () => {
+    const classes = useStyles()
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    Something test
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default CardsContainer
